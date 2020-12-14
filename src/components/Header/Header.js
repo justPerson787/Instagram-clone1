4e9cea36d070a95c9ba2328b6344739ae48db713
N.U <n.n.ushakova@gmail.com>
2020-12-14 01:58:51 -0500

    Add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+    <nav>
+        <div>
+            <a href="/">
+                Instagram
+            </a>
+        </div>
+    </nav>
+);
+
+export default Header;
