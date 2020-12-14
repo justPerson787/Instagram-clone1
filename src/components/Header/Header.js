06b674fb976a429361dc58945eaa19e6ecf0bb7b
N.U <n.n.ushakova@gmail.com>
2020-12-14 11:08:34 -0500

    Style Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => (
-    <nav>
-        <div>
-            <a href="/">
-                Instagram
-            </a>
+    <nav className="Nav">
+        <div className="Nav-menus">
+            <div className="Nav-brand">
+                <a className="Nav-brand-logo" href="/">
+                    
+                </a>
+            </div>
         </div>
     </nav>
 );
