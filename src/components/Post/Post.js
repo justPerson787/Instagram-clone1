b8b3634a5636fa66b8a1c331e6fba029e49a4b95
N.U <n.n.ushakova@gmail.com>
2020-12-16 09:57:50 -0500

    Correct styles

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -7,11 +7,9 @@ class Post extends React.Component {
         <article className="Post" ref="Post">
             <header>
                 <div className = "Post-user">
-                    <div className="Post-user-avatar"><img src='../../avatar.jpg' alt='avatar' /> </div>
-                </div>
-                <div className="Post-user-nickname">
-                    <span>Stranger</span>
-                </div>
+                    <div className="Post-user-avatar"><img src="https://images.unsplash.com/photo-1607798136809-1483b83f32fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80" alt='avatar' /> </div>
+                    <span className="Post-user-nickname">Stranger</span>
+                </div>                
             </header>
             <div className="Post-image">
                 <div className="Post-image-bg">
@@ -20,6 +18,7 @@ class Post extends React.Component {
             </div>
             <div className="Post-caption">
                 <strong>Stranger</strong> is here!
+                <img src="avatar.jpg" />
             </div>
         </article>
       )
