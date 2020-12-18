407a2ffd413bbf49c92110f33af9e8d983ba54c5
N.U <n.n.ushakova@gmail.com>
2020-12-18 10:57:18 -0500

    Add server data rendering

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,35 +1,60 @@
 import React from 'react';
 import "./Post.css";
-//import avatar from "./../../avatar.jpg";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
-class Post extends React.Component {
-    constructor(props){
-        super(props);
-    }
-    render() {
-      const nickname = this.props.nickname;
-      const avatar = this.props.avatar;
-      const image = this.props.image;
-      const caption = this.props.caption;
-      return (
-        <article className="Post" ref="Post">
-            <header>
-                <div className = "Post-user">
-                    <div className="Post-user-avatar"><img src={avatar} alt='avatar' /> </div>
-                    <span className="Post-user-nickname">{nickname}</span>
-                </div>                
-            </header>
-            <div className="Post-image">
-                <div className="Post-image-bg">
-                    <img alt={caption} src={image} />
-                </div>
-            </div>
-            <div className="Post-caption">
-                <strong>{nickname}</strong> is here!                
-            </div>
-        </article>
-      )
-    };
-}
+// src/components/Post/index.js
+const Post = () => {
+    return (
+        <Query
+        query={gql`
+            {
+            post(user_id: "a", post_id: "a") {
+                image
+                caption
+                user {
+                nickname
+                avatar
+                }
+            }
+            }
+        `}
+        >
+        // handle result of the query
+        {({ loading, error, data }) => {
+            if (loading) return <p>Loading Post...</p>;
+            if (error) return <p>Error loading Post:(</p>;
+            let image = data.post.image;
+            let caption = data.post.caption;
+            let user = data.post.user;
 
+            //JSX to be rendered 
+            return (
+                <article className="Post" ref="Post">
+                  <header>
+                    <div className="Post-user">
+                      <div className="Post-user-avatar">
+                        <img src={user.avatar} alt={user.nickname} />
+                      </div>
+                      <div className="Post-user-nickname">
+                        <span>{user.nickname}</span>
+                      </div>
+                    </div>
+                  </header>
+                  <div className="Post-image">
+                    <div className="Post-image-bg">
+                      <img alt={caption} src={image} />
+                    </div>
+                  </div>
+                  <div className="Post-caption">
+                    <strong>{user.nickname}</strong> {caption}
+                  </div>
+                </article>
+            );
+        }}
+        </Query>
+    );
+};
+
+        
 export default Post;
